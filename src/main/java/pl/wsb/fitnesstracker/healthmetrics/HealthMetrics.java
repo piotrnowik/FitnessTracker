2d42aa30cc7130ee06bc1bbc69936ea120b9ecc8
vdi-terminal <piotr.nowikow@gmail.com>
2026-03-15 17:20:59 +0100

    LAB01: Dodano tabelę Health_metrics

---
+++ src/main/java/pl/wsb/fitnesstracker/healthmetrics/HealthMetrics.java
@@ -0,0 +1,46 @@
+package pl.wsb.fitnesstracker.healthmetrics;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import pl.wsb.fitnesstracker.user.api.User;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "health_metrics")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class HealthMetrics {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
+
+    private int user_id;
+
+    private int date;
+    private int weight;
+    private int height;
+    private int heartRate;
+
+
+    public HealthMetrics(
+            final int user,
+            final int date,
+            final int weight,
+            final int height,
+            final int heartRate) {
+        this.user_id = user;
+        this.date = date;
+        this.weight = weight;
+        this.height = height;
+        this.heartRate = heartRate;
+    }
+
+}
